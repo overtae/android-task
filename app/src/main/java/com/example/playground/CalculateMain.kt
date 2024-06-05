95e61f9dc1796361bb5225f5a6cbb3f3f6a51295
taeyoung <rlaxodud911@gmail.com>
2024-06-05 11:29:02 +0900

    :sparkles: feat: Lv2 구현

--- app/src/main/java/com/example/playground/CalculateMain.kt
+++ app/src/main/java/com/example/playground/CalculateMain.kt
@@ -3,9 +3,21 @@ package com.example.playground
 fun main() {
     val calculator = Calculator()
 
-    println("1 + 2 = ${calculator.add(1, 2)}")
-    println("1 - 2 = ${calculator.sub(1, 2)}")
-    println("1 * 2 = ${calculator.mul(1, 2)}")
-    println("1 / 2 = ${calculator.divide(1, 2)}")
-    println("1 / 0 = ${calculator.divide(1, 0)}")
+    println(calculator.add(1, 2) + calculator.sub(3, 4))
+
+    println("계산하실 숫자를 공백으로 구분하여 입력해주세요.")
+    val numbers = getInput().split(" ").map(String::toInt)
+
+    calculator.remain(numbers[0], numbers[1])
+}
+
+fun getInput(): String {
+    do {
+        readlnOrNull()?.let {
+            when {
+                it.replace(" ", "").toInt() != null -> return it
+                else -> println("올바른 값을 입력해주세요.")
+            }
+        }
+    } while (true)
 }
