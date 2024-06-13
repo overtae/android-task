6e57a59774acd7da8cd6c988acbb9441f48347b1
taeyoung <rlaxodud911@gmail.com>
2024-06-13 20:55:15 +0900

    :recycle: refactor: Food > SubMenu로 변경

--- app/src/main/java/com/example/playground/KioskMain.kt
+++ app/src/main/java/com/example/playground/KioskMain.kt
@@ -18,30 +18,30 @@ fun init() {
         burgers, frozenCustard, drinks, beer,
 
         // burgers
-        Food(burgers.name, "ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"),
-        Food(burgers.name, "SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"),
-        Food(burgers.name, "Shroom Burger", 9.4, "몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거"),
-        Food(burgers.name, "Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"),
-        Food(burgers.name, "Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"),
+        SubMenu(burgers.name, "ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"),
+        SubMenu(burgers.name, "SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"),
+        SubMenu(burgers.name, "Shroom Burger", 9.4, "몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거"),
+        SubMenu(burgers.name, "Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"),
+        SubMenu(burgers.name, "Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"),
 
         // frozenCustard
-        Food(frozenCustard.name, "Mango Shake", 7.8, "망고 퓨레와 바닐라 커스터드가 어우러진 쉐이크"),
-        Food(frozenCustard.name, "Classic Shakes", 6.8, "진한 커스터드가 들어간 클래식 쉐이크"),
-        Food(frozenCustard.name, "Floats", 6.8, "바닐라 커스터드와 탄산이 만난 음료"),
-        Food(frozenCustard.name, "Cup & Cones", 5.7, "쫀득하고 진한 아이스크림"),
+        SubMenu(frozenCustard.name, "Mango Shake", 7.8, "망고 퓨레와 바닐라 커스터드가 어우러진 쉐이크"),
+        SubMenu(frozenCustard.name, "Classic Shakes", 6.8, "진한 커스터드가 들어간 클래식 쉐이크"),
+        SubMenu(frozenCustard.name, "Floats", 6.8, "바닐라 커스터드와 탄산이 만난 음료"),
+        SubMenu(frozenCustard.name, "Cup & Cones", 5.7, "쫀득하고 진한 아이스크림"),
 
         // drinks
-        Food(drinks.name, "Americano", 4.5, "물이 들어간 커피"),
-        Food(drinks.name, "Latte", 5.0, "우유를 넣은 커피"),
-        Food(drinks.name, "Caramel Latte", 5.9, "캬라멜 소스와 우유를 넣은 커피"),
-        Food(drinks.name, "Vanilla Latte", 5.5, "바닐라 시럽과 우유를 넣은 커피"),
-        Food(drinks.name, "Cappuccino", 5.5, "우유 거품이 들어간 커피"),
+        SubMenu(drinks.name, "Americano", 4.5, "물이 들어간 커피"),
+        SubMenu(drinks.name, "Latte", 5.0, "우유를 넣은 커피"),
+        SubMenu(drinks.name, "Caramel Latte", 5.9, "캬라멜 소스와 우유를 넣은 커피"),
+        SubMenu(drinks.name, "Vanilla Latte", 5.5, "바닐라 시럽과 우유를 넣은 커피"),
+        SubMenu(drinks.name, "Cappuccino", 5.5, "우유 거품이 들어간 커피"),
 
         // beer
-        Food(beer.name, "Cass", 6.5, "카스 생맥주"),
-        Food(beer.name, "Terra", 6.5, "테라 생맥주"),
-        Food(beer.name, "Krush", 7.0, "카리나가 광고한 맥주"),
-        Food(beer.name, "Asahi", 7.0, "아사히 생맥주"),
+        SubMenu(beer.name, "Cass", 6.5, "카스 생맥주"),
+        SubMenu(beer.name, "Terra", 6.5, "테라 생맥주"),
+        SubMenu(beer.name, "Krush", 7.0, "카리나가 광고한 맥주"),
+        SubMenu(beer.name, "Asahi", 7.0, "아사히 생맥주"),
     )
 
     menu.map { menuList.add(it) }
@@ -49,7 +49,7 @@ fun init() {
 
 fun displayMenu() {
     while (true) {
-        val (food, menu) = menuList.partition { it is Food }
+        val (food, menu) = menuList.partition { it is SubMenu }
         val maxMenuLength = menu.maxOfOrNull { it.name.length } ?: 0
 
         println("\"SHAKESHACK BURGER 에 오신걸 환영합니다.\"")
@@ -64,22 +64,22 @@ fun displayMenu() {
         // 대분류 메뉴 입력
         val selectedMenuNumber = getInput(0..menu.size).also { if (it == 0) return }
         val selectedMenu = menu[selectedMenuNumber - 1]
-        val filteredFood = food.filter { (it as Food).category == selectedMenu.name }
-        val maxFoodLength = filteredFood.maxOfOrNull { it.name.length } ?: 0
+        val filteredSubMenu = food.filter { (it as SubMenu).category == selectedMenu.name }
+        val maxFoodLength = filteredSubMenu.maxOfOrNull { it.name.length } ?: 0
 
         println("[ ${selectedMenu.name} MENU ]")
-        filteredFood.mapIndexed { i, item ->
+        filteredSubMenu.mapIndexed { i, item ->
             print("${i + 1}. ")
             item.displayInfo(maxFoodLength)
         }
         println("0. ${"뒤로가기".padEnd(maxFoodLength, ' ')}| 뒤로가기\n")
 
         // 소분류 메뉴 입력
-        val selectedFoodNumber = getInput(0..filteredFood.size)
+        val selectedFoodNumber = getInput(0..filteredSubMenu.size)
 
         if (selectedFoodNumber == 0) continue
         else {
-            val selectedFood = filteredFood[selectedFoodNumber - 1]
+            val selectedFood = filteredSubMenu[selectedFoodNumber - 1]
             println("선택하신 메뉴를 확인해주세요.")
             selectedFood.displayInfo(maxFoodLength)
             println()
--- app/src/main/java/com/example/playground/SubMenu.kt
+++ app/src/main/java/com/example/playground/SubMenu.kt
@@ -1,6 +1,6 @@
 package com.example.playground
 
-class Food(val category: String, name: String, val price: Double, description: String) :
+class SubMenu(val category: String, name: String, val price: Double, description: String) :
     Menu(name, description) {
     override fun displayInfo(formatLength: Int) {
         println("${name.padEnd(formatLength, ' ')}   | w $price | $description")
