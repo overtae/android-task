a8c7ef62baf961a03cbf803a0593f854b1de21df
taeyoung <rlaxodud911@gmail.com>
2024-06-20 19:55:01 +0900

    :sparkles: feat: 랜덤 이미지 표시 기능 추가

--- app/src/main/java/com/example/androidtask/HomeActivity.kt
+++ app/src/main/java/com/example/androidtask/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.androidtask
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
+    val colors = mapOf(
+        "blue" to R.drawable.blue_flower,
+        "pink" to R.drawable.pink_flower,
+        "purple" to R.drawable.purple_flower,
+        "red" to R.drawable.red_flower,
+        "yellow" to R.drawable.yellow_flower
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,14 +29,26 @@ class HomeActivity : AppCompatActivity() {
         }
 
         val id = intent.getStringExtra("id") ?: ""
+        val user = findUserById(id)
 
         val idText = findViewById<TextView>(R.id.tv_id)
+        val nameText = findViewById<TextView>(R.id.tv_name)
+
+        val image = findViewById<ImageView>(R.id.iv_image)
         val exitButton = findViewById<Button>(R.id.btn_exit)
 
-        idText.text = "아이디: $id"
+        idText.text = getString(R.string.id_info, user?.id)
+        nameText.text = getString(R.string.name_info, user?.name)
 
         exitButton.setOnClickListener {
             finish()
         }
+
+        val color = colors.keys.random()
+        image.setImageResource(colors[color]!!)
+    }
+
+    private fun findUserById(id: String): User? {
+        return users.find { it.id == id }
     }
 }
