fc3706a1d3b029c0485561a56ffc9c5540d51ed2
taeyoung <rlaxodud911@gmail.com>
2024-06-20 11:15:17 +0900

    :sparkles: feat: 홈 페이지 디자인 및 기능 구현

--- app/src/main/java/com/example/androidtask/HomeActivity.kt
+++ app/src/main/java/com/example/androidtask/HomeActivity.kt
@@ -1,20 +1,33 @@
-package com.example.playground
+package com.example.androidtask
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val id = intent.getStringExtra("id") ?: ""
+
+        val idText = findViewById<TextView>(R.id.tv_id)
+        val exitButton = findViewById<Button>(R.id.btn_exit)
+
+        idText.text = "아이디: $id"
+
+        exitButton.setOnClickListener {
+            finish()
         }
     }
 }
