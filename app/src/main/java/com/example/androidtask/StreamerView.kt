08d145684d1a4a890534725be1a9824b2b3e49c9
taeyoung <rlaxodud911@gmail.com>
2024-07-01 18:36:08 +0900

    :construction: feature: 커스텀 스트리머 뷰 구현

---
+++ app/src/main/java/com/example/androidtask/StreamerView.kt
@@ -0,0 +1,34 @@
+package com.example.androidtask
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.ResourcesCompat
+
+class StreamerView(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+    ConstraintLayout(context, attrs, defStyleAttr) {
+    private var ivProfile: ImageView
+    private var tvName: TextView
+
+    init {
+        val view = LayoutInflater.from(context).inflate(R.layout.streamer_view, this, false)
+        addView(view)
+
+        tvName = findViewById(R.id.tv_streamer_name)
+        ivProfile = findViewById(R.id.iv_streamer_profile)
+    }
+
+    fun setProfile(imgRes: Int) {
+        ivProfile.setImageResource(imgRes)
+        ivProfile.background =
+            ResourcesCompat.getDrawable(resources, R.drawable.container_circle, null)
+        ivProfile.clipToOutline = true
+    }
+
+    fun setName(text: String) {
+        tvName.text = text
+    }
+}
---
+++ app/src/main/java/com/example/androidtask/holder/StreamerHolder.kt
@@ -0,0 +1,25 @@
+package com.example.androidtask.holder
+
+import android.view.View
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidtask.R
+import com.example.androidtask.StreamerView
+import com.example.androidtask.data.Streamer
+
+
+class StreamerHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    fun bindData(streamer: List<Streamer>) {
+        view.apply {
+            val layout = findViewById<LinearLayout>(R.id.linear_streamer)
+
+            streamer.map { s ->
+                val streamerView = StreamerView(view.context).apply {
+                    setProfile(s.profileImg)
+                    setName(s.name)
+                }
+                layout.addView(streamerView)
+            }
+        }
+    }
+}
