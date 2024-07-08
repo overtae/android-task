b33f74483fa79ce357261d365db19f8b9beab8f7
taeyoung <rlaxodud911@gmail.com>
2024-07-08 13:24:41 +0900

    :sparkles: feature: 메인 화면과 업로드 화면 연결

--- app/src/main/java/com/example/androidtask/MainActivity.kt
+++ app/src/main/java/com/example/androidtask/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.androidtask
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,9 +14,11 @@ import com.example.androidtask.data.DataManager
 import com.example.androidtask.data.Header
 import com.example.androidtask.data.ListDataType
 import com.example.androidtask.data.ListDataWrapper
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
-    private val mainItems: ArrayList<ListDataWrapper> = arrayListOf()
+    private val suggestionItems: ArrayList<ListDataWrapper> = arrayListOf()
+    private val followingItems: ArrayList<ListDataWrapper> = arrayListOf()
     private lateinit var mainAdapter: MainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,45 +32,81 @@ class MainActivity : AppCompatActivity() {
         }
 
         DataManager.init(this)
+        initSuggestionItems()
+        initFollowingItems()
         init()
+
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab!!.position) {
+                    0 -> init(suggestionItems)
+                    1 -> init(followingItems)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+
+        findViewById<ImageButton>(R.id.btn_search).setOnClickListener {
+            startActivity(Intent(this@MainActivity, SearchActivity::class.java))
+        }
+
+        findViewById<ImageButton>(R.id.btn_profile).setOnClickListener {
+            startActivity(Intent(this@MainActivity, UploadActivity::class.java))
+        }
     }
 
-    private fun init() {
-        initItems()
-
+    private fun init(item: ArrayList<ListDataWrapper> = suggestionItems) {
         val rvMain = findViewById<RecyclerView>(R.id.rv_main)
-        mainAdapter = MainAdapter(mainItems)
+        mainAdapter = MainAdapter(item)
         rvMain.adapter = mainAdapter
         rvMain.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 
-    private fun initItems() {
-        mainItems.add(ListDataWrapper(ListDataType.TYPE_LARGE_LIVE, DataManager.lgLiveList))
+    private fun initSuggestionItems() {
+        suggestionItems.add(ListDataWrapper(ListDataType.TYPE_LARGE_LIVE, DataManager.lgLiveList))
 
         // small live list
-        mainItems.add(
+        suggestionItems.add(
             ListDataWrapper(ListDataType.TYPE_HEADER, Header("이 방송 어때요?", ""))
         )
         DataManager.commonLiveList.map {
-            mainItems.add(ListDataWrapper(ListDataType.TYPE_SMALL_LIVE, it))
+            suggestionItems.add(ListDataWrapper(ListDataType.TYPE_SMALL_LIVE, it))
         }
 
         // category list
-        mainItems.add(
+        suggestionItems.add(
             ListDataWrapper(ListDataType.TYPE_HEADER, Header("좋아하실 것 같아요", ""))
         )
-        mainItems.add(ListDataWrapper(ListDataType.TYPE_CATEGORY, DataManager.categoryList))
+        suggestionItems.add(ListDataWrapper(ListDataType.TYPE_CATEGORY, DataManager.categoryList))
 
         // medium live list
-        mainItems.add(
+        suggestionItems.add(
             ListDataWrapper(ListDataType.TYPE_HEADER, Header("리그 오브 레전드 추천 라이브", "리그 오브 레전드"))
         )
-        mainItems.add(ListDataWrapper(ListDataType.TYPE_MEDIUM_LIVE, DataManager.commonLiveList))
+        suggestionItems.add(
+            ListDataWrapper(
+                ListDataType.TYPE_MEDIUM_LIVE,
+                DataManager.commonLiveList
+            )
+        )
 
         // partner streamer list
-        mainItems.add(
+        suggestionItems.add(
             ListDataWrapper(ListDataType.TYPE_HEADER, Header("파트너 스트리머", ""))
         )
-        mainItems.add(ListDataWrapper(ListDataType.TYPE_STREAMER, DataManager.streamerList))
+        suggestionItems.add(ListDataWrapper(ListDataType.TYPE_STREAMER, DataManager.streamerList))
+    }
+
+    private fun initFollowingItems() {
+        // online streamer
+        DataManager.commonLiveList.map {
+            followingItems.add(ListDataWrapper(ListDataType.TYPE_SMALL_LIVE, it))
+        }
+
+        // offline streamer
+        followingItems.add(ListDataWrapper(ListDataType.TYPE_HEADER, Header("오프라인", "")))
     }
 }
--- app/src/main/java/com/example/androidtask/UploadActivity.kt
+++ app/src/main/java/com/example/androidtask/UploadActivity.kt
@@ -32,7 +32,7 @@ class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_search)
+        setContentView(R.layout.activity_upload)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
