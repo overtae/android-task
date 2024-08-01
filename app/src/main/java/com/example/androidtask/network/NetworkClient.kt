94613f77efa4a1bf4e80265b51620b61755ce7ae
taeyoung <rlaxodud911@gmail.com>
2024-08-01 14:30:56 +0900

    :sparkles: feature: API 인터페이스 정의 및 Retrofit 인스턴스 생성

---
+++ app/src/main/java/com/example/androidtask/network/NetworkClient.kt
@@ -0,0 +1,44 @@
+package com.example.androidtask.network
+
+import com.example.androidtask.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okio.IOException
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.jvm.Throws
+
+object NetworkClient {
+    private const val BASE_URL = "https://dapi.kakao.com/v2/search/"
+    val kakaoNetwork: NetworkInterface by lazy { getClient().create(NetworkInterface::class.java) }
+
+    private fun getClient(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(getOkHttpClient(NetworkInterceptor()))
+            .build()
+    }
+
+    private fun getOkHttpClient(interceptor: NetworkInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .run {
+                addInterceptor(interceptor)
+                addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                })
+                build()
+            }
+    }
+
+    class NetworkInterceptor : Interceptor {
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+            val newRequest = chain.request().newBuilder()
+                .addHeader("Authorization", "KakaoAK ${BuildConfig.KAKAO_API_KEY}")
+                .build()
+            return chain.proceed(newRequest)
+        }
+    }
+}
